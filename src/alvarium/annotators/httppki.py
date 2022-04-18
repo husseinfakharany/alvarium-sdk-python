6d2544d7b66d0333ee87351cf95626f821e914b2
ramezmoussa <ramez.moussa@aucegypt.edu>
2022-04-18 04:16:53 -0700

    implement: HTTPPKI annotator

--- src/alvarium/annotators/httppki.py
+++ src/alvarium/annotators/httppki.py
@@ -1,12 +1,13 @@
 import socket
+import os
 
 from alvarium.sign.exceptions import SignException
 from alvarium.sign.factories import SignProviderFactory
 from alvarium.contracts.annotation import Annotation, AnnotationType
 from alvarium.hash.contracts import HashType
-from alvarium.sign.contracts import KeyInfo, SignInfo, KeyInfo
+from alvarium.sign.contracts import KeyInfo, SignInfo, KeyInfo, SignType
 from alvarium.utils import PropertyBag
-from .contracts import Signable, parseSignature
+from .contracts import Signable
 from .utils import derive_hash, sign_annotation
 from .interfaces import Annotator
 from .exceptions import AnnotatorException
@@ -26,11 +27,28 @@ class HttpPkiAnnotator(Annotator):
         except SignException as e:
             raise AnnotatorException("cannot get sign provider.", e)
         
+        if(not os.path.isfile(key.path)):
+            raise AnnotatorException("Cannot read Public Key File.")
+
         with open(key.path, 'r') as file:
             pub_key = file.read()
-            return sign_provider.verify(key=bytes.fromhex(pub_key), 
+            
+            try:
+                hex_pub_key = bytes.fromhex(pub_key)
+            except Exception as e:
+                raise AnnotatorException("Cannot read Public Key File.")
+
+            try:
+                hex_signature = bytes.fromhex(signable.signature)
+            except:
+                raise AnnotatorException("Cannot verify signature.")
+
+
+
+            return  sign_provider.verify(key=hex_pub_key, 
                                         content=bytes(signable.seed, 'utf-8'),
-                                        signed=bytes.fromhex(signable.signature))
+                                        signed=hex_signature)
+
 
 
     def execute(self, data: bytes, ctx: PropertyBag = None) -> Annotation:
@@ -38,36 +56,39 @@ class HttpPkiAnnotator(Annotator):
         host: str = socket.gethostname()
 
         # call parser on request
-        req = ctx['request']
+        req = ctx.get_property('request')
+
         try:
             parsed_data = parseSignature(req)
-
         except AnnotatorException as e:
             raise AnnotatorException("cannot parse the http request.", e)
 
 
-        # create Signable object
-        signable = Signable()
-        signable.seed = parsed_data.seed
-        siganble.signature = parsed_data.signature
+        if(parsed_data.signature == ""):
+            raise AnnotatorException("Signature is empty.")
 
-        k = KeyInfo()
-        directory = self.sign_info.public.path
-        k.path = "/".join(directory, parsed_data.keyid)
+        # create Signable object
+        signable = Signable(parsed_data.seed, parsed_data.signature)
+
+
 
         try:
-            k.type = SignType(parsed_data.algorithm)
+            signType = SignType(parsed_data.algorithm)
         except SignException as e:
             raise AnnotatorException("Invalid key type specified" + str(parsed_data.algorithm), e)
 
+        k = KeyInfo(signType, parsed_data.keyid)
+
+
         try:
-            self.verify_signature(k, signable)
-        except AnnotatorException as e:
-            raise AnnotatorException("cannot verify signature.", e)
+            is_satisfied = self._verify_signature(k, signable)
+        except Exception as e:
+            raise AnnotatorException(str(e))
         
 
         annotation = Annotation(key=key, host=host, hash=self.hash, kind=self.kind, is_satisfied=is_satisfied)
-                
+        
+
         signature: str = sign_annotation(key_info=self.sign_info.private, annotation=annotation)
         annotation.signature = signature
         return annotation
